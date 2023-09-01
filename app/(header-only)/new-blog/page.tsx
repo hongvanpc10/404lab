6bc562fa2090639a95bdda7b5abc54f7ad40acea
hongvanpc10 <hongvanpc10@gmail.com>
2023-09-01 13:36:33 +0700

    rtghrt

--- app/(header-only)/new-blog/page.tsx
+++ app/(header-only)/new-blog/page.tsx
@@ -88,6 +88,10 @@ export default function NewBlog() {
 					</label>
 					<TextEditor />
 				</div>
+
+				<button className='px-8 py-3 !mt-16 mr-4 transition hover:bg-amber-600 bg-emerald-500'>
+					Đăng bài viết
+				</button>
 			</form>
 		</div>
 	)
