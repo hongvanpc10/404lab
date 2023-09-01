ebdbfac427f6a787632adf723878eace48a98280
hongvanpc10 <hongvanpc10@gmail.com>
2023-09-01 11:54:30 +0700

    sdfbdf

--- components/input.tsx
+++ components/input.tsx
@@ -4,7 +4,7 @@ export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
 	return (
 		<input
 			{...props}
-			className='outline-none w-full bg-gray-900 py-4 px-10 [clip-path:polygon(2%_0,98%_0,100%_70%,98%_100%,2%_100%,0%_30%)]'
+			className='outline-none w-full bg-gray-900 py-4 px-10 [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_70%,calc(100%-1rem)_100%,1rem_100%,0%_30%)]'
 		/>
 	)
 }
--- components/textarea.tsx
+++ components/textarea.tsx
@@ -6,7 +6,7 @@ export default function Textarea(
 	return (
 		<textarea
 			{...props}
-			className='outline-none w-full bg-gray-900 py-5 px-8 [clip-path:polygon(2%_0,98%_0,100%_70%,98%_100%,2%_100%,0%_30%)]'
+			className='outline-none w-full bg-gray-900 py-5 px-8 [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_70%,calc(100%-1rem)_100%,1rem_100%,0%_30%)]'
 		/>
 	)
 }
